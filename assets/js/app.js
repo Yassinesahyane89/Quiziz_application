19e36e95bfcf233e3687e36c51fc58b9acdfc8f7
Yassinesahyane89 <yassine.sahyane89@gmail.com>
2022-12-27 06:30:41 +0100

    result page

--- assets/js/app.js
+++ assets/js/app.js
@@ -140,3 +140,60 @@ function getSelected() {
 // function myStopFunction() {
 //     clearInterval(myInterval);
 // }
+
+/* ========================== Result page ==========================  */
+let quizresult = document.querySelector(".result_quiz");
+let questionEl_corr = document.getElementById("question_corr");
+let answer_a_corr = document.getElementById("answer_a_corr");
+let answer_b_corr = document.getElementById("answer_b_corr");
+let answer_c_corr = document.getElementById("answer_c_corr");
+let answer_d_corr = document.getElementById("answer_d_corr");
+let explan_corr = document.getElementById("explan_corr");
+
+function Quizresult(){
+    Titlepage.innerHTML = "Result Quiz ";
+    Questionner.style.display = "none";
+    tableresult.style.display = "block";
+    for (let i = 0; i < quizData.length; i++) {
+        let result;
+        if (Arrayanswer[i].answer == quizData[i].correct) {
+        result = true;
+        } else {
+        result = false;
+        }
+        quizresult.innerHTML += `
+                            <tr>
+                                <td>Question - ${i + 1} </td>
+                                <td id="useranswer">${
+                                        Arrayanswer[i].answer
+                                }</td>
+                                <td>${result}</td>
+                                <td><button class="d-flex align-items-center border-0 border-top" data-bs-toggle="modal" data-bs-target="#modal" onclick="Quizdetail(${Arrayanswer[i].answer},${i})"  data-id="${i}">View detail</button></td>
+                            </tr>
+                            `;
+    }
+}
+
+function Quizdetail(useranswer,id) {
+    Resetquizdetail();
+    const currentQuizData = quizData[id];
+    questionEl_corr.innerText = currentQuizData.question;
+    answer_a_corr.innerText = currentQuizData.a;
+    answer_b_corr.innerText = currentQuizData.b;
+    answer_c_corr.innerText = currentQuizData.c;
+    answer_d_corr.innerText = currentQuizData.d;
+    document.getElementById(currentQuizData.correct+"_corr").checked = true;
+    document.getElementById(currentQuizData.correct+"_corr_answer").style.backgroundColor = "green";
+    userquiz = useranswer.getAttribute("id");
+    if (currentQuizData.correct != userquiz) {
+        document.getElementById(userquiz + "_corr_answer").style.backgroundColor ="red";
+    }
+        explan_corr.innerText = currentQuizData.explan;
+}
+
+function Resetquizdetail() {
+    document.getElementById("a_corr_answer").style.backgroundColor = "white";
+    document.getElementById("b_corr_answer").style.backgroundColor = "white";
+    document.getElementById("c_corr_answer").style.backgroundColor = "white";
+    document.getElementById("d_corr_answer").style.backgroundColor = "white";
+}
