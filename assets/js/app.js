247a0cda407208405865e19f9017e38800b8d92a
Yassinesahyane89 <yassine.sahyane89@gmail.com>
2022-12-27 06:27:31 +0100

    Question page

--- assets/js/app.js
+++ assets/js/app.js
@@ -42,3 +42,101 @@ function update() {
     Quizresult();
   }
 }
+
+/* ========================== Quiz page ==========================  */
+let answerlabel = document.querySelectorAll(".answer");
+let questions = document.getElementById("question");
+let answer_a = document.getElementById("answer_a");
+let answer_b = document.getElementById("answer_b");
+let answer_c = document.getElementById("answer_c");
+let answer_d = document.getElementById("answer_d");
+let currentquestion = document.getElementById("questionNumber");
+let totalquestion = document.getElementById("total_question");
+let submitBtn = document.getElementById("submitQuiz");
+// let seconds = document.getElementById("second_time");
+
+let prog = document.getElementById("prog");
+
+
+let currentQuiz = 0;
+let score = 0;
+// let timeanswer = 30;
+// let myInterval;
+let Arrayanswer=[];
+
+totalquestion.innerText = quizData.length;
+
+
+function Showdata() {
+
+     prog.style.width = ((currentQuiz + 1) / quizData.length) * 100 + "%";
+
+    Titlepage.innerHTML = "AWS Cloud Practitioner Knowledge Test ";
+
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+    currentquestion.innerText = currentQuiz + 1;
+    questions.innerText = currentQuizData.question;
+    answer_a.innerText = currentQuizData.a;
+    answer_b.innerText = currentQuizData.b;
+    answer_c.innerText = currentQuizData.c;
+    answer_d.innerText = currentQuizData.d;
+}
+
+submitBtn.addEventListener("click", () => {
+    const answer = getSelected();
+    if (answer) {
+
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+        Arrayanswer.push({ index: currentQuiz, answer: answer });
+        console.log(Arrayanswer);
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            Showdata();
+        }else{
+            ProgressBarCheck();
+        }
+    }
+});
+
+function deselectAnswers() {
+    answerlabel.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerlabel.forEach((answerEl) => {
+        if (answerEl.checked) {
+        answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+/* ======================== Start_Time_counter ======================== */
+// function countdown() {
+//     timeanswer--;
+//     seconds.innerHTML = formatTime(timeanswer);
+//     if (timeanswer == 0) {
+//         myStopFunction();
+//         Arrayanswer.push({ index : currentQuiz, answer :"Noanswer" });
+//         currentQuiz++;
+//         Showdata();
+//     }
+// }
+
+// function formatTime(time) {
+//     return time < 10 ? `0${time}` : time;
+// }
+
+// function myStopFunction() {
+//     clearInterval(myInterval);
+// }
